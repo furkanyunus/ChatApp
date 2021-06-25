445a94bd8c79b93e224efe5633081b18d114c4d7
Furkan Yunus <tangut_furkan@hotmail.com>
2021-06-25 17:53:00 +0300

    First initialize of the project :tada:

---
+++ app/src/main/java/sohbet/chatiste/ImageViewerActivity.java
@@ -0,0 +1,27 @@
+package sohbet.chatiste;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+import sohbet.chatiste.R;
+
+public class ImageViewerActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private String imageUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_viewer);
+
+        imageView=findViewById(R.id.image_viewer);
+        imageUrl=getIntent().getStringExtra("url");
+
+        Picasso.get().load(imageUrl).into(imageView);
+    }
+}
---
+++ app/src/main/java/sohbet/chatiste/TabsAccessorAdapter.java
@@ -0,0 +1,72 @@
+package sohbet.chatiste;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class TabsAccessorAdapter extends FragmentPagerAdapter
+{
+
+    public TabsAccessorAdapter(FragmentManager fm)
+    {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+
+            case 1:
+                groupsFragment groups = new groupsFragment();
+                return groups;
+
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+
+            default:
+                return null;
+        }
+    }
+
+
+    @Override
+    public int getCount()
+    {
+        return 4;
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        switch (position)
+        {
+            case 0:
+                return "SOHBETLER";
+
+            case 1:
+                return "GRUPLAR";
+
+            case 2:
+                return "ARKADAŞLAR";
+
+            case 3:
+                return "İstekler";
+
+            default:
+                return null;
+        }
+    }
+}
